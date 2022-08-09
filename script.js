f98ebebc734a279d237a517ee1e534f289ed23c1
Diana Savchuk <101061426+DianaSavchuk@users.noreply.github.com>
2022-08-09 14:08:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 var images;
 
 
-images = ['https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-03-22_13-55-58.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-03-31_18-32-23.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-08-09_11-16-13.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-08-09_11-17-50.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/IMG_2534.JPG?raw=true'];
 
+images = ['https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-03-22_13-55-58.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-03-31_18-32-23.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-08-09_11-16-13.jpg?raw=true','https://github.com/DianaSavchuk/code-pen-landingpage/blob/main/photo_2022-08-09_11-17-50.jpg?raw=true'];
 let element_image = document.getElementById('image');
 element_image.setAttribute("src", images[0]);
 
