0f1c9b2bdcf6ab7aaa32d12576bd377285ac0691
Diana Savchuk <101061426+DianaSavchuk@users.noreply.github.com>
2022-08-09 12:59:09 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/* =================
+  TESTS, LOOK AT THESE
+  Reading tests will always help you discover your requirements.
+  You can make this window bigger. 
+   ===================
+*/
+
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
